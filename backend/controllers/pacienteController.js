e3d2f0aebd6395305294a05a561278f5ea32eaba
JuaannMS <jmms.android@gmail.com>
2023-06-07 00:49:48 -0400

    crud paciente

--- backend/controllers/pacienteController.js
+++ backend/controllers/pacienteController.js
@@ -3,8 +3,44 @@ const Paciente = require('../models/paciente');
 const createPaciente = async (req, res) => {
     try {
       const { rut, nombre, direccion, edad, saldo } = req.body;
+
+      const regex = /^[A-Za-z\s]+$/;
+
+      if (!regex.test(nombre)) {
+      return res.status(400).send({ message: "El nombre debe contener solo letras" });
+      }
+
+      if (nombre.length < 8 ){
+      return res.status(400).send({ message: "El nombre no contiene los suficientes caracteres" });
+      }
+
+      if (edad<0 || edad > 150){
+      return res.status(400).send({ message: "La edad es incorrecta" });
+      }
+
+      if (saldo < 0){
+      return res.status(400).send({ message: "El saldo es incorrecto" });
+      }
+
+      const regexRut = /^[\d.kK-]+$/;
+      if (!regexRut.test(rut)) {
+      return res.status(400).send({ message: "El rut no puede contener letras" });
+      }
+
+      if (rut.length < 8 || rut.length > 12){
+        return res.status(400).send({ message: "El rut es incorrecto" });
+      }
+
+      if (direccion.length < 8){
+        return res.status(400).send({ message: "La direccion no contiene los suficientes caracteres" });
+      }
+
+      if (await Paciente.exists({ rut: rut })) {
+        return res.status(400).send({ message: "El rut ya fue ingresado" });
+      }
+
       const newPaciente = new Paciente({
-        rut,
+        rut:rut,
         nombre,
         direccion,
         edad,
@@ -19,10 +55,8 @@ const createPaciente = async (req, res) => {
     }
   };
 
-
+//modificar
 const getPaciente = (req,res) => {
-
-    console.log('holaaa')
 
     const {idPaciente} = req.body
     Paciente.findById(idPaciente, (error,paciente) => {
@@ -37,14 +71,15 @@ const getPaciente = (req,res) => {
     })
 }
 
+
 const getPacientes = async (req, res) => {
     try {
       const pacientes = await Paciente.find().exec();
-  
+
       if (pacientes.length === 0) {
         return res.status(404).send({ message: "No se han encontrado pacientes registrados" });
       }
-  
+
       return res.status(200).send(pacientes);
     } catch (error) {
       return res.status(400).send({ message: "No se realizó la búsqueda" });
@@ -52,8 +87,43 @@ const getPacientes = async (req, res) => {
   };
 
 
+  const updatePaciente = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const paciente = await Paciente.findByIdAndUpdate(id, req.body, { new: true });
+
+      if (!paciente) {
+        return res.status(404).send({ message: "No se encontró el paciente" });
+      }
+
+      return res.status(200).send(paciente);
+    } catch (error) {
+      return res.status(400).send({ message: "No se actualizaron los datos del paciente" });
+    }
+  };
+
+
+  const deletePaciente = async (req, res) => {
+
+    try{
+      const { id } = req.params
+      const paciente = await Paciente.findByIdAndDelete(id);
+
+      if (!paciente) {
+        return res.status(404).send({ message: "No se encontró el paciente" });
+      }
+
+      return res.status(200).send({ message: "Paciente eliminado" });
+    } catch (error) {
+      return res.status(400).send({ message: "No se elimino al paciente" });
+    }
+  };
+
+
 module.exports = {
     createPaciente,
     getPaciente,
-    getPacientes
+    getPacientes,
+    updatePaciente,
+    deletePaciente
 }
--- backend/routes/pacienteRoutes.js
+++ backend/routes/pacienteRoutes.js
@@ -5,6 +5,8 @@ const api = express.Router();
 api.post('/paciente', pacienteController.createPaciente);
 api.get('/getPaciente/:id', pacienteController.getPaciente)
 api.get('/pacientes', pacienteController.getPacientes)
+api.put('/paciente/:id', pacienteController.updatePaciente)
+api.delete('/deletePaciente/:id', pacienteController.deletePaciente)
 
 module.exports = api;
 
