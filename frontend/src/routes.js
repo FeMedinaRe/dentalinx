91d9be809ec853128f0a6d74e3cdbd3e562e1ad7
BenjaJara98 <benjamin.jara1701@alumnos.ubiobio.cl>
2023-08-04 19:56:29 -0400

    crud citas

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -30,58 +30,67 @@ import Typography from "views/Typography/Typography.js";
 import Icons from "views/Icons/Icons.js";
 import Maps from "views/Maps/Maps.js";
 import NotificationsPage from "views/Notifications/Notifications.js";
+import ReserveAttention from "views/ReserveAttention/ReserveAttention.js";
+
 // core components/views for RTL layout
 
-const dashboardRoutes = [
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    icon: Dashboard,
-    component: DashboardPage,
-    layout: "/admin",
-  },
-  {
-    path: "/user",
-    name: "Registrar Pacientes",
-    icon: Person,
-    component: UserProfile,
-    layout: "/admin",
-  },
-  {
-    path: "/table",
-    name: "Listado Pacientes",
-    icon: "content_paste",
-    component: TableList,
-    layout: "/admin",
-  },
-  {
-    path: "/typography",
-    name: "Typography",
-    icon: LibraryBooks,
-    component: Typography,
-    layout: "/admin",
-  },
-  {
-    path: "/icons",
-    name: "Icons",
-    icon: BubbleChart,
-    component: Icons,
-    layout: "/admin",
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    icon: LocationOn,
-    component: Maps,
-    layout: "/admin",
-  },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    icon: Notifications,
-    component: NotificationsPage,
-    layout: "/admin",
-  },
+const dashboardRoutes = [{
+        path: "/dashboard",
+        name: "Dashboard",
+        icon: Dashboard,
+        component: DashboardPage,
+        layout: "/admin",
+    },
+    {
+        path: "/user",
+        name: "Registrar Pacientes",
+        icon: Person,
+        component: UserProfile,
+        layout: "/admin",
+    },
+    {
+        path: "/table",
+        name: "Listado Pacientes",
+        icon: "content_paste",
+        component: TableList,
+        layout: "/admin",
+    },
+    {
+        path: "/typography",
+        name: "Typography",
+        icon: LibraryBooks,
+        component: Typography,
+        layout: "/admin",
+    },
+    {
+        path: "/icons",
+        name: "Icons",
+        icon: BubbleChart,
+        component: Icons,
+        layout: "/admin",
+    },
+    {
+        path: "/maps",
+        name: "Maps",
+        icon: LocationOn,
+        component: Maps,
+        layout: "/admin",
+    },
+    {
+        path: "/notifications",
+        name: "Notifications",
+        icon: Notifications,
+        component: NotificationsPage,
+        layout: "/admin",
+    },
+
+    {
+        path: "/reserveattention",
+        name: "Reservar Hora",
+        icon: Notifications,
+        component: ReserveAttention,
+        layout: "/admin",
+    },
 ];
 
 export default dashboardRoutes;
