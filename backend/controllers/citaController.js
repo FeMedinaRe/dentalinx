de5dee82a6e735910c02f511b27d18e771d5d3f8
BenjaJara98 <benjamin.jara1701@alumnos.ubiobio.cl>
2023-08-09 02:27:27 -0400

    limpieza de codigo

--- backend/controllers/citaController.js
+++ backend/controllers/citaController.js
@@ -6,7 +6,6 @@ const tratamiento = require("../models/tratamiento");
 //Para crear una nueva cita
 const createCita = async(req, res) => {
     try {
-        console.log("Hola");
         var { paciente_id, dentista_id, tratamiento_id, fecha, hora, estado } = req.body;
 
         var _id_paciente = paciente_id;
@@ -15,30 +14,15 @@ const createCita = async(req, res) => {
 
         //PACIENTE
         paciente_id = await paciente.findOne({ nombre: _id_paciente }, { _id: 1 })
-            // console.log("AYUDA");
         paciente_id = paciente_id._id;
-        // console.log("ALOHA");
-        console.log(paciente_id);
 
         //DENTISTA
         dentista_id = await dentista.findOne({ nombre: _id_dentista }, { _id: 1 })
-        console.log("DENTISTA");
         dentista_id = dentista_id._id;
-        console.log(dentista_id);
 
         //TRATAMIENTO
         tratamiento_id = await tratamiento.findOne({ nombre: _id_tratamiento }, { _id: 1 })
-        console.log("TRATAMIENTO");
         tratamiento_id = tratamiento_id._id;
-        console.log(tratamiento_id);
-
-        // console.log(paciente_id);
-        // console.log(dentista_id);
-        // console.log(tratamiento_id);
-        // console.log(typeof fecha);
-        // console.log(typeof hora);
-        // console.log(typeof estado);
-
 
         citaNueva = new Cita({
             paciente_id,
@@ -50,7 +34,6 @@ const createCita = async(req, res) => {
         })
 
         citaNueva = citaNueva.save();
-
 
         return res.status(201).send(citaNueva);
     } catch (error) {
@@ -90,7 +73,7 @@ const getPacientes = async(req, res) => {
     }
 }
 
-//Para obtener los dentistas para el select
+//Para obtener los tratamientos para el select
 const getTratamientos = async(req, res) => {
     try {
         const tratamientos = await tratamiento.find();
@@ -172,8 +155,6 @@ const getCitas = async(req, res) => {
 
 const updateCita = async(req, res) => {
     try {
-
-        console.log("PRUEBA UPDATE");
         var { id } = req.params;
 
         var { paciente_id, dentista_id, tratamiento_id, fecha, hora, estado } = req.body;
@@ -194,16 +175,6 @@ const updateCita = async(req, res) => {
         tratamiento_id = await tratamiento.findOne({ nombre: _id_tratamiento }, { _id: 1 })
         tratamiento_id = tratamiento_id._id;
 
-        console.log("Este es el id que llega");
-        console.log(id);
-
-        console.log("Lo transformamos a ObjectId")
-        const { ObjectId } = require("mongodb");
-
-        // id = new ObjectId(id)
-        // console.log(typeof id);
-        // console.log(id);
-
         var query = await Cita.findByIdAndUpdate (id, {
             paciente_id: paciente_id,
             dentista_id: dentista_id,
@@ -215,15 +186,12 @@ const updateCita = async(req, res) => {
             new: true,
         });
 
-        console.log(query);
-
         if (!query) {
             return res.status(404).send({ message: "No se encontró la cita" });
         }
 
         return res.status(201).send(query);
     }  catch (error) {
-        console.log("Ocurrió un error:", error.message);
         return res.status(400).send({ message: error.message });
     }
 };
