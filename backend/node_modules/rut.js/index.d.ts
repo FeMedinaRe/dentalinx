d346118db0ad5387bb0c7e3987132dcd3978d43b
FeMedinaRe <65627242+FeMedinaRe@users.noreply.github.com>
2024-09-03 22:44:05 +0000

    new file:   .env.example 	modified:   .gitignore eliminado node_modules

--- backend/node_modules/rut.js/index.d.ts
+++
@@ -1,7 +0,0 @@
-export function clean(rut: string): string;
-
-export function validate(rut: string): boolean;
-
-export function format(rut: string, options?: { dots: boolean }): string;
-
-export function getCheckDigit(rut: string): string;
